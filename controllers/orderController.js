0ad7f85df424ca1c6afd6bfd69a7a9d5cb703e58
Jay Patel <000881881@mohawkcollege.ca>
2024-05-23 19:37:25 -0400

    final commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,59 +1,73 @@
 import Order from "../models/orderModel.js";
 import Product from "../models/productModel.js";
 
-//utility function
-const calcPrices = (orderItems) => {
+// Utility Function
+function calcPrices(orderItems) {
   const itemsPrice = orderItems.reduce(
     (acc, item) => acc + item.price * item.qty,
     0
   );
+
   const shippingPrice = itemsPrice > 100 ? 0 : 10;
-  const taxPrice = 0.13 * itemsPrice;
-  const totalPrice = (itemsPrice + shippingPrice + taxPrice).tofixed(2);
+  const taxRate = 0.15;
+  const taxPrice = (itemsPrice * taxRate).toFixed(2);
+
+  const totalPrice = (
+    itemsPrice +
+    shippingPrice +
+    parseFloat(taxPrice)
+  ).toFixed(2);
+
   return {
     itemsPrice: itemsPrice.toFixed(2),
     shippingPrice: shippingPrice.toFixed(2),
     taxPrice,
     totalPrice,
   };
-};
+}
 
 const createOrder = async (req, res) => {
   try {
     const { orderItems, shippingAddress, paymentMethod } = req.body;
+
     if (orderItems && orderItems.length === 0) {
-      res.status(400).json({ error: "No order items" });
+      res.status(400);
+      throw new Error("No order items");
     }
+
     const itemsFromDB = await Product.find({
-      _id: { $in: orderItems.map((item) => item.product) },
+      _id: { $in: orderItems.map((x) => x._id) },
     });
 
-    const dbOrderItems = itemsFromDB.map((item) => {
-      const matchingItemsFromDB = orderItems.find(
-        (orderItem) => orderItem._id.toString() === item._id
+    const dbOrderItems = orderItems.map((itemFromClient) => {
+      const matchingItemFromDB = itemsFromDB.find(
+        (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
       );
-      if (!matchingItemsFromDB) {
-        return res.status(400).json({ error: `Item ${item.name} not found` });
+
+      if (!matchingItemFromDB) {
+        res.status(404);
+        throw new Error(`Product not found: ${itemFromClient._id}`);
       }
 
       return {
-        ...item,
-        product: item._id,
-        price: matchingItemsFromDB.price,
+        ...itemFromClient,
+        product: itemFromClient._id,
+        price: matchingItemFromDB.price,
         _id: undefined,
       };
     });
 
-    const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
       calcPrices(dbOrderItems);
+
     const order = new Order({
       orderItems: dbOrderItems,
       user: req.user._id,
       shippingAddress,
       paymentMethod,
       itemsPrice,
-      shippingPrice,
       taxPrice,
+      shippingPrice,
       totalPrice,
     });
 
@@ -66,7 +80,7 @@ const createOrder = async (req, res) => {
 
 const getAllOrders = async (req, res) => {
   try {
-    const orders = await Order.find({}).populate("user", "name");
+    const orders = await Order.find({}).populate("user", "id username");
     res.json(orders);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -82,7 +96,7 @@ const getUserOrders = async (req, res) => {
   }
 };
 
-const getTotalOrders = async (req, res) => {
+const countTotalOrders = async (req, res) => {
   try {
     const totalOrders = await Order.countDocuments();
     res.json({ totalOrders });
@@ -91,44 +105,59 @@ const getTotalOrders = async (req, res) => {
   }
 };
 
-const getTotalSales = async (req, res) => {
+const calculateTotalSales = async (req, res) => {
   try {
     const orders = await Order.find();
-    const totalSales = orders.reduce((acc, order) => acc + order.totalPrice, 0);
+    const totalSales = orders.reduce((sum, order) => sum + order.totalPrice, 0);
     res.json({ totalSales });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const getTotalSalesByDate = async (req, res) => {
+const calcualteTotalSalesByDate = async (req, res) => {
   try {
-    const { date } = req.query;
-    const orders = await Order.find({ createdAt: { $gte: date } });
-    const totalSales = orders.reduce((acc, order) => acc + order.totalPrice, 0);
-    res.json({ totalSales });
+    const salesByDate = await Order.aggregate([
+      {
+        $match: {
+          isPaid: true,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: { format: "%Y-%m-%d", date: "$paidAt" },
+          },
+          totalSales: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+
+    res.json(salesByDate);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const getOrderById = async (req, res) => {
+const findOrderById = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id).populate(
       "user",
-      "name email"
+      "username email"
     );
+
     if (order) {
       res.json(order);
     } else {
-      res.status(404).json({ error: "Order not found" });
+      res.status(404);
+      throw new Error("Order not found");
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const payOrder = async (req, res) => {
+const markOrderAsPaid = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
     if (order) {
@@ -136,9 +165,9 @@ const payOrder = async (req, res) => {
       order.paidAt = Date.now();
       order.paymentResult = {
         id: req.body.id,
-        status: req.body.status,
-        update_time: req.body.update_time,
-        email_address: req.body.payer.email_address,
+        // status: req.body.status,
+        // update_time: req.body.update_time,
+        // email_address: req.body.payer.email_address,
       };
       const updatedOrder = await order.save();
       res.status(200).json(updatedOrder);
@@ -150,16 +179,19 @@ const payOrder = async (req, res) => {
   }
 };
 
-const deliverOrder = async (req, res) => {
+const markOrderAsDelivered = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
+
     if (order) {
       order.isDelivered = true;
       order.deliveredAt = Date.now();
+
       const updatedOrder = await order.save();
-      res.status(200).json(updatedOrder);
+      res.json(updatedOrder);
     } else {
-      res.status(404).json({ error: "Order not found" });
+      res.status(404);
+      throw new Error("Order not found");
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -170,10 +202,10 @@ export {
   createOrder,
   getAllOrders,
   getUserOrders,
-  getTotalOrders,
-  getTotalSales,
-  getTotalSalesByDate,
-  getOrderById,
-  payOrder,
-  deliverOrder,
+  countTotalOrders,
+  calculateTotalSales,
+  calcualteTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
 };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,18 +1,19 @@
 import express from "express";
 const router = express.Router();
 
-import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import {
   createOrder,
   getAllOrders,
   getUserOrders,
-  getTotalOrders,
-  getTotalSales,
-  getTotalSalesByDate,
-  getOrderById,
-  payOrder,
-  deliverOrder,
+  countTotalOrders,
+  calculateTotalSales,
+  calcualteTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
 } from "../controllers/orderController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 router
   .route("/")
@@ -20,16 +21,13 @@ router
   .get(authenticate, authorizeAdmin, getAllOrders);
 
 router.route("/mine").get(authenticate, getUserOrders);
-router.route("/total-orders").get(authenticate, authorizeAdmin, getTotalOrders);
-router.route("/total-sales").get(authenticate, authorizeAdmin, getTotalSales);
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
+router.route("/total-sales-by-date").get(calcualteTotalSalesByDate);
+router.route("/:id").get(authenticate, findOrderById);
+router.route("/:id/pay").put(authenticate, markOrderAsPaid);
 router
-  .route("/total-sales-by-date")
-  .get(authenticate, authorizeAdmin, getTotalSalesByDate);
-
-router.route("/:id").get(authenticate, getOrderById);
-
-router.route("/:id/pay").put(authenticate, payOrder);
-
-router.route("/:id/deliver").put(authenticate, authorizeAdmin, deliverOrder);
+  .route("/:id/deliver")
+  .put(authenticate, authorizeAdmin, markOrderAsDelivered);
 
 export default router;
