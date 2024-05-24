4c21a64ff8a0fce6d981bf47797411f3f4bf524a
Jay Patel <000881881@mohawkcollege.ca>
2024-05-24 00:00:20 -0400

    fix cookie issue

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,14 +1,18 @@
 import jwt from "jsonwebtoken";
+
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 
-  // set jwt as an http-only cookie
-  res.cookie("jwt", token, {
-    secure: process.env.NODE_ENV === "development",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  });
+  res
+    .writeHead(200, {
+      "Set-Cookie": "token=encryptedstring; HttpOnly",
+      "Access-Control-Allow-Credentials": "true",
+    })
+    .send();
+
   return token;
 };
+
 export default generateToken;
