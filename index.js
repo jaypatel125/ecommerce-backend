5124f41b117808ebc85100eda829e28a9f7d08cc
Jay Patel <jaykoladia@gmail.com>
2024-05-24 22:09:15 -0400

    add cors options

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://ecom-frontend-gamma.vercel.app",
     credentials: true,
   })
 );
