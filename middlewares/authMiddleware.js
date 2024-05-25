71015bbff2fccb94dd72471b8e9ad0bf7d41807f
Jay Patel <jaykoladia@gmail.com>
2024-05-25 00:43:41 -0400

    update middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,23 +4,22 @@ import asyncHandler from "./asyncHandler.js";
 
 const authenticate = asyncHandler(async (req, res, next) => {
   let token;
-  // read jwt from cookie
 
-  if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  }
-  if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
-  } else {
+  // Read JWT from the 'jwt' cookie
+  token = req.cookies.jwt;
+
+  if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
       res.status(401);
-      throw new Error("Not authorized, token failed");
+      throw new Error("Not authorized, token failed.");
     }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token.");
   }
 });
 
